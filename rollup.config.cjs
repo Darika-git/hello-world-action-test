f0804307639f10658a905f1b9f1f8ab5d65d6dbf
Darika Sophea <darikasophea2@gmail.com>
2026-06-20 18:12:53 +0700

    Public Action Test

---
+++ rollup.config.cjs
@@ -0,0 +1,16 @@
+const commonjs = require("@rollup/plugin-commonjs");
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+
+module.exports = {
+  input: "src/index.js",
+
+  output: {
+    file: "dist/index.js",
+    format: "cjs"
+  },
+
+  plugins: [
+    nodeResolve({ preferBuiltins: true }),
+    commonjs()
+  ]
+};
